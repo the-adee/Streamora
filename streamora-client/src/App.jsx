48e09499bf55bfe7b8ad2356d51c234b7dd0a96f
the-great-adee <adityagupta46772@gmail.com>
2025-09-08 23:44:55 +0530

    feat(realtime): establish initial client-server connection via SOCKET.IO

--- streamora-client/src/App.jsx
+++ streamora-client/src/App.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const SERVER_URL = "http://localhost:3001";
+
+const socket = io(SERVER_URL);
 
 import "./App.css";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  useEffect(() => {
+    function onConnect() {
+      console.log(`Connected to Socket.IO server! Your ID is`, socket.id);
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      console.log(`Disconnected from SOCKET.IO server`);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
   return (
     <>
       <h1>Building Streamora</h1>
@@ -10,6 +34,18 @@ function App() {
       <h4>
         <i>in production</i>
       </h4>
+      <div className="mt-8 p-4 rounded-md bg-gray-700 w-full max-w-sm">
+        <p className="text-xl font-semibold">
+          Connection Status&nbsp;<i>(Socket.IO)</i>
+        </p>
+        <p
+          className={`mt-2 text-2xl font-bold ${
+            isConnected ? "text-green-400" : "text-red-500"
+          }`}
+        >
+          {isConnected ? "Connected" : "Disconnected"}
+        </p>
+      </div>
     </>
   );
 }
--- streamora-server/src/index.js
+++ streamora-server/src/index.js
@@ -2,17 +2,35 @@
 // contains server logic not the application logic -> (middlewares, routes)
 // easier for testing
 
+import http from "http";
+import { Server } from "socket.io";
 import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected. Socket ID: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected. Socket ID: ${socket.id}`);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
-
+server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
   //   local route to open server '/'
   console.log(`Local: http://localhost:${PORT}/`);
-  
+  console.log(`Socket.IO server is waiting for connections...`);
 });
